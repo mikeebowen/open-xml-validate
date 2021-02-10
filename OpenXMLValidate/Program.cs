20eb7c417af387659eae9464704516caa596aba0
Mike Bowen <mibowe@microsoft.com>
2021-02-10 11:31:43 -0800

    initial commit

---
+++ OpenXMLValidate/Program.cs
@@ -0,0 +1,36 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Validation;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+
+namespace inspect_it
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the path to the OOXML file");
+            string path = Console.ReadLine();
+
+            OpenPresentation(path);
+        }
+        public static void OpenPresentation(string fileName)
+        {
+            //PresentationDocument doc = PresentationDocument.Open(fileName, false);
+            if (File.Exists(fileName))
+            {
+                WordprocessingDocument doc = WordprocessingDocument.Open(fileName, false);
+                OpenXmlValidator openXmlValidator = new OpenXmlValidator();
+                IEnumerable<ValidationErrorInfo> validations = openXmlValidator.Validate(doc);
+                foreach (ValidationErrorInfo validationErrorInfo in validations)
+                {
+                    Console.WriteLine($"Validation Error: {validationErrorInfo.Description}");
+                    Console.WriteLine($"Validation XPath: {validationErrorInfo.Path.XPath}");
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#open-xml-validate
+
+This is a simple .net core command line app that will print the errors in a docx file to the console.
